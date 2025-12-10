058ecce021970bf0a6d51a7636fce5d2c0e1cd77
IshanMNG <ishan.mishra@mng.ch>
2025-12-10 11:50:29 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -12,20 +12,11 @@ CREATE TABLE todos (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE bewertung (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sterne VARCHAR(250) NOT NULL,
-    datum VARCHAR(250) NOT NULL,
-    schueler_id VARCHAR(250) NOT NULL,
-    lehrer_id VARCHAR(250) NOT NULL
-    FOREIGN KEY (schüler_id) REFRENCES schüler(id),
-    FOREIGN KEY (lehrer_id) REFRENCES schüler(id)
-);
-
 CREATE TABLE schueler (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(250) NOT NULL,
-    benutzername VARCHAR(250) NOT NULL,
+    username VARCHAR(250) NOT NULL UNIQUE,
+    password VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE lehrer (
@@ -34,4 +25,14 @@ CREATE TABLE lehrer (
     name VARCHAR(250) NOT NULL,
     vorname VARCHAR(250) NOT NULL,
     fach VARCHAR(250) NOT NULL,
+);
+
+CREATE TABLE bewertung (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sterne VARCHAR(250) NOT NULL,
+    datum VARCHAR(250) NOT NULL,
+    schueler_id VARCHAR(250) NOT NULL,
+    lehrer_id VARCHAR(250) NOT NULL
+    FOREIGN KEY (schueler_id) REFERENCES schueler(id),
+    FOREIGN KEY (lehrer_id) REFERENCES lehrer(id)
 );
