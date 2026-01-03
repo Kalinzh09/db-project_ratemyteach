940e9a10b839821fb227da3a8ec0168e2f49f1e5
Kalinzh09 <kalin.vckovski@mng.ch>
2026-01-03 11:39:18 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -5,10 +5,12 @@ from db import db_read, db_write
 from auth import login_manager, authenticate, register_user
 from flask_login import login_user, logout_user, login_required, current_user
 import logging
-
+ 
 # --------------------
 # Setup
 # --------------------
+
+
 logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 
@@ -16,12 +18,32 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.secret_key = "supersecret"
 
+# DON'T CHANGE
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
+
+# DON'T CHANGE
+@app.post('/update_server')
+def webhook():
+    x_hub_signature = request.headers.get('X-Hub-Signature')
+    if is_valid_signature(x_hub_signature, request.data, W_SECRET):
+        repo = git.Repo('./mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    return 'Unathorized', 401
+
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
 # --------------------
 # Auth
 # --------------------
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
