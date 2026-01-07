ebd21c7e3b64f0478b20f7efb3223bf7e973dab1
IshanMNG <ishan.mishra@mng.ch>
2026-01-07 11:56:05 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,111 +3,77 @@ from flask_login import LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import db_read, db_write
 
-# Logger für dieses Modul
 logger = logging.getLogger(__name__)
-
 login_manager = LoginManager()
 
 
 class User(UserMixin):
-    def __init__(self, id, username, password):
+    def __init__(self, id, username, email, password):
         self.id = id
         self.username = username
+        self.email = email
         self.password = password
 
     @staticmethod
     def get_by_id(user_id):
-        logger.debug("User.get_by_id() aufgerufen mit user_id=%s", user_id)
-        try:
-            row = db_read(
-                "SELECT * FROM users WHERE id = %s",
-                (user_id,),
-                single=True
-            )
-            logger.debug("User.get_by_id() DB-Ergebnis: %r", row)
-        except Exception:
-            logger.exception("Fehler bei User.get_by_id(%s)", user_id)
-            return None
-
+        row = db_read(
+            "SELECT * FROM schueler WHERE id = %s",
+            (user_id,),
+            single=True
+        )
         if row:
-            return User(row["id"], row["username"], row["password"])
-        else:
-            logger.warning("User.get_by_id(): kein User mit id=%s gefunden", user_id)
-            return None
+            return User(row["id"], row["username"], row["email"], row["password"])
+        return None
 
     @staticmethod
     def get_by_username(username):
-        logger.debug("User.get_by_username() aufgerufen mit username=%s", username)
-        try:
-            row = db_read(
-                "SELECT * FROM users WHERE username = %s",
-                (username,),
-                single=True
-            )
-            logger.debug("User.get_by_username() DB-Ergebnis: %r", row)
-        except Exception:
-            logger.exception("Fehler bei User.get_by_username(%s)", username)
-            return None
-
+        row = db_read(
+            "SELECT * FROM schueler WHERE username = %s",
+            (username,),
+            single=True
+        )
         if row:
-            return User(row["id"], row["username"], row["password"])
-        else:
-            logger.info("User.get_by_username(): kein User mit username=%s", username)
-            return None
-
-
-# Flask-Login
-@login_manager.user_loader
-def load_user(user_id):
-    logger.debug("load_user() aufgerufen mit user_id=%s", user_id)
-    try:
-        user = User.get_by_id(int(user_id))
-    except ValueError:
-        logger.error("load_user(): user_id=%r ist keine int", user_id)
+            return User(row["id"], row["username"], row["email"], row["password"])
         return None
 
-    if user:
-        logger.debug("load_user(): User gefunden: %s (id=%s)", user.username, user.id)
-    else:
-        logger.warning("load_user(): kein User für id=%s gefunden", user_id)
 
-    return user
+@login_manager.user_loader
+def load_user(user_id):
+    try:
+        return User.get_by_id(int(user_id))
+    except ValueError:
+        return None
 
 
-# Helpers
-def register_user(username, password):
-    logger.info("register_user(): versuche neuen User '%s' anzulegen", username)
+def register_user(username, password, email):
+    """
+    Registriert einen neuen Schüler in der DB-Tabelle `schueler`.
+    E-Mail ist **Pflicht**.
+    """
+    if not email:
+        logger.warning("register_user(): Keine E-Mail angegeben")
+        return False
 
-    existing = User.get_by_username(username)
-    if existing:
+    if User.get_by_username(username):
         logger.warning("register_user(): Username '%s' existiert bereits", username)
         return False
 
-    hashed = generate_password_hash(password)
+    hashed_pw = generate_password_hash(password)
+
     try:
         db_write(
-            "INSERT INTO users (username, password) VALUES (%s, %s)",
-            (username, hashed)
+            "INSERT INTO schueler (username, email, password) VALUES (%s, %s, %s)",
+            (username, email, hashed_pw)
         )
-        logger.info("register_user(): User '%s' erfolgreich angelegt", username)
-    except Exception:
-        logger.exception("Fehler beim Anlegen von User '%s'", username)
+        logger.info("register_user(): Benutzer '%s' erfolgreich angelegt", username)
+        return True
+    except Exception as e:
+        logger.exception("Fehler beim Anlegen von Benutzer '%s': %s", username, e)
         return False
-
-    return True
 
 
 def authenticate(username, password):
-    logger.info("authenticate(): Login-Versuch für '%s'", username)
     user = User.get_by_username(username)
-
-    if not user:
-        logger.warning("authenticate(): kein User mit username='%s' gefunden", username)
-        return None
-
-    if check_password_hash(user.password, password):
-        logger.info("authenticate(): Passwort korrekt für '%s'", username)
+    if user and check_password_hash(user.password, password):
         return user
-
-    logger.warning("authenticate(): falsches Passwort für '%s'", username)
     return None
